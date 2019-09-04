16569c0efae715a36a40597f3fe7c8e7afde967c
Brett Fodor <bmattmusic@gmail.com>
2019-09-04 11:41:04 -0400

    First Commit

---
+++ GitExampleOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GitExampleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Brett";
+
+            string lastName = "Fodor";
+            
+            int age = 29;
+
+            int puttingOnSomeYears = 40;
+
+            int oldAge = age + puttingOnSomeYears;
+
+            Console.WriteLine(firstName + " " + lastName);
+
+            Console.WriteLine("I am " + age + " years old");
+
+            Console.WriteLine("I'm really glad I'm not " + oldAge + " years old");
+        }
+    }
+}
